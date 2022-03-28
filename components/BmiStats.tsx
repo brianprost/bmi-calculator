c5f1e4acae2c87eeef5987952c527f4f1a53af09
Brian Prost <brian.s.prost@gmail.com>
2022-03-28 14:31:14 -0400

    assigned key for BmiStats

--- components/BmiStats.tsx
+++ components/BmiStats.tsx
@@ -6,20 +6,22 @@ const BmiStats = () => {
     <>
       <div className="stats flex flex-wrap shadow sm:flex-none sm:flex-nowrap">
         {bmiData.map(({ name, range, color }) => (
-          <div className="stat">
+          <div className="stat" key={name}>
             <div className="stat-title text-slate-900">{name}</div>
             <div className={`stat-value ${color}`}>{range}</div>
           </div>
         ))}
       </div>
-      <p className="text-md text-bmi-lemon tracking-wide">
-        {"Source: "}
+      <p className="text-md tracking-wide text-bmi-lemon">
+        {'Source: '}
         <a
           href="https://www.nhlbi.nih.gov/health/educational/lose_wt/BMI/bmicalc.htm"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className='underline text-bmi-yellow'>National Heart, Lung, and Blood Institute</span>
+          <span className="text-bmi-yellow underline">
+            National Heart, Lung, and Blood Institute
+          </span>
         </a>
       </p>
     </>
