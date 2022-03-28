4f55739c58b8853fe509ca634ac2cd885035dd79
Brian Prost <brian.s.prost@gmail.com>
2022-03-28 14:33:06 -0400

    added favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,76 @@ const Home: NextPage = () => {
       <Head>
         <title>BMI Calculator</title>
         <meta name="robots" content="noindex" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* For old IEs */}
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+
+        {/* For new browsers - multisize ico  */}
+        <link
+          rel="icon"
+          type="image/x-icon"
+          sizes="16x16 32x32"
+          href="/favicon/favicon.ico"
+          key={'multisize-favicon'}
+        />
+
+        {/* For iPad with high-resolution Retina display running iOS ≥ 7: */}
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/favicon/favicon-152-precomposed.png"
+        />
+
+        {/* For iPad with high-resolution Retina display running iOS ≤ 6: */}
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/favicon/favicon-144-precomposed.png"
+        />
+
+        {/* For iPhone with high-resolution Retina display running iOS ≥ 7: */}
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/favicon/favicon-120-precomposed.png"
+        />
+
+        {/* For iPhone with high-resolution Retina display running iOS ≤ 6: */}
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/favicon/favicon-114-precomposed.png"
+        />
+
+        {/* For iPhone 6+ */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/favicon-180-precomposed.png"
+        />
+
+        {/* For first- and second-generation iPad: */}
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/favicon/favicon-72-precomposed.png"
+        />
+
+        {/* For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: */}
+        <link rel="apple-touch-icon" sizes="57x57" href="/favicon/favicon-57.png" />
+
+        {/* For IE10 Metro */}
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta name="msapplication-TileImage" content="favicon-144.png" />
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* Chrome for Android */}
+        <link rel="manifest" href="manifest.json" />
+        <link
+          rel="icon"
+          sizes="192x192"
+          href="/favicon/favicon-192.png"
+          key={'chrome for android'}
+        />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-evenly bg-bmi-blue px-20 text-center font-dela">
